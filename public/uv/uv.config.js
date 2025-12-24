9af61c85e52fe6a2c075d4083ef317af6e575320
classroom84x-games <Classroom84X@outlook.com>
2025-12-24 18:50:40 -0500

    Add Ultraviolet configuration file

---
+++ public/uv/uv.config.js
@@ -0,0 +1,10 @@
+self.__uv$config = {
+  prefix: '/service/',
+  bare: 'http://145.223.79.168:3000',
+  encodeUrl: Ultraviolet.codec.xor.encode,
+  decodeUrl: Ultraviolet.codec.xor.decode,
+  handler: '/uv/uv.handler.js',
+  bundle: '/uv/uv.bundle.js',
+  config: '/uv/uv.config.js',
+  sw: '/uv/uv.sw.js',
+};
